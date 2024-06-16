8f84e42657df620378cd49c52513910f9b77ab02
Benoit Masson <benoit.masson@gmail.com>
2024-06-16 21:43:39 +0200

    Refactor: extract dot significance in a dedicated function

--- internal/extract/contents.go
+++ internal/extract/contents.go
@@ -11,42 +11,51 @@ func ReadBits(dots [][]bool, maskID MaskID) []bool {
 
 	direction := -1 // start moving up
 	for j := s - 1; j >= 1; j -= 2 {
+		if j == 6 {
+			// vertical version column exception: ignore totally and move to previous column
+			j--
+		}
 		start, end := 0, s
 		if direction < 0 {
 			start, end = s-1, -1
 		}
-		for i := start; i != end; i += direction {
-			if j == 6 || i == 6 {
-				// ignore version information
-				if j == 6 {
-					// vertical version column exception: ignore totally and move to previous column
-					j--
-				}
-				continue
-			}
-			if (i <= 8 && j <= 8) ||
-				(i <= 8 && j >= s-8) ||
-				(i >= s-8 && j <= 8) {
-				// ignore 3 finder markers in the corners + format info
-				continue
-			}
-			if i >= s-9 && i <= s-5 &&
-				j >= s-8 && j <= s-5 {
-				// ignore alignment pattern in the bottom-right
-				// works only for a single pattern, i.e. version 6 and below
-				continue
-			}
 
-			if i >= s-9 && i <= s-5 &&
-				j == s-9 {
-				// ignore alignment pattern in the bottom-right - special case where 2-bits column is half-cut
-			} else {
-				bits = append(bits, dots[i][j] != mask(i, j)) // != is the same as XOR for booleans
+		for i := start; i != end; i += direction {
+			for offset := 0; offset <= 1; offset++ {
+				if !isSignificantDot(i, j-offset, s) {
+					continue
+				}
+
+				bits = append(bits, dots[i][j-offset] != mask(i, j-offset)) // != is the same as XOR for booleans
 			}
-			bits = append(bits, dots[i][j-1] != mask(i, j-1)) // != is the same as XOR for booleans
 		}
 		direction *= -1 // invert direction
 	}
 
 	return bits
+}
+
+// isSignificantDot returns whether dot at position (i, j) represents a valid message bit,
+// and not a specific pattern (marker, version, …)
+func isSignificantDot(i, j, size int) bool {
+	if (i <= 8 && j <= 8) ||
+		(i <= 8 && j >= size-8) ||
+		(i >= size-8 && j <= 8) {
+		// ignore 3 finder markers in the corners + format info + dark spot
+		return false
+	}
+
+	if i >= size-9 && i <= size-5 &&
+		j >= size-9 && j <= size-5 {
+		// ignore alignment pattern in the bottom-right
+		// works only for a single pattern, i.e. version 6 and below
+		return false
+	}
+
+	if j == 6 || i == 6 {
+		// ignore version information
+		return false
+	}
+
+	return true
 }
